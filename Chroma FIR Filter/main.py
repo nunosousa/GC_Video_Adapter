a32813223fe7abcd4bcbe6e1be1fc92e0b9fbc3e
Nuno Sousa <nunosousa@outlook.com>
2020-12-22 18:36:55 +0000

    Update main.py

--- Chroma FIR Filter/main.py
+++ Chroma FIR Filter/main.py
@@ -5,8 +5,25 @@ import matplotlib.pyplot as pyplot
 FIR_Coefs = np.array([1300, -420, 236, -152, 104, -70, 48, -32, 20, -12, 6, -4])
 FIR_Norm_Coef = 2048
 
-# Number of non-zero samples
+# Number of (downsampled) samples
 Test_Sample_size = 100
 
 # Test vector
-Test_Sample_in = np.zeros(Test_Sample_size*2 + FIR_Coefs.size*2)
+rng = np.random.default_rng()
+rand_seq = np.absolute(rng.standard_normal(Test_Sample_size))
+Test_Sample_in = np.divide(rand_seq, np.amax(rand_seq))*255
+
+# Number of head and tail buffer samples
+Paddind_Head_size = (FIR_Coefs.size*2)-1
+Padding_Tail_Size = FIR_Coefs.size*2
+
+
+
+#Test_Sample_in = np.zeros(Test_Sample_size*2 + Paddind_Head_size + Padding_Tail_Size)
+
+#Test_Sample_in[0:(FIR_Coefs.size*2)-1] =
+print(Test_Sample_in)
+
+pyplot.figure(1)
+pyplot.plot(Test_Sample_in)
+pyplot.show()
