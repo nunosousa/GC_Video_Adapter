6343839698af01d1aa1fabc0a9bcfff83f19a33b
Nuno Sousa <nunosousa@outlook.com>
2020-12-23 00:20:24 +0000

    Update main.py

--- Chroma FIR Filter/main.py
+++ Chroma FIR Filter/main.py
@@ -6,7 +6,7 @@ FIR_Coefs = np.array([1300, -420, 236, -152, 104, -70, 48, -32, 20, -12, 6, -4])
 FIR_Norm_Coef = 2048
 
 # Number of (downsampled) samples
-Test_Sample_size = 10
+Test_Sample_size = 5
 
 # Test vector
 rng = np.random.default_rng()
@@ -16,8 +16,8 @@ Test_Sample_in = np.floor(np.divide(rand_seq, np.amax(rand_seq))*255)
 # Number of head and tail buffer samples
 Paddind_Head_size = (FIR_Coefs.size*2)-2
 Padding_Tail_Size = FIR_Coefs.size*2-1
-Padding_Head = np.ones(Paddind_Head_size)
-Padding_Tail = np.ones(Padding_Tail_Size)
+Padding_Head = np.ones(Paddind_Head_size)*0
+Padding_Tail = np.ones(Padding_Tail_Size)*0
 
 # Build output vector
 Test_Sample_out = np.zeros(Test_Sample_size*2 + Paddind_Head_size + Padding_Tail_Size)
@@ -38,10 +38,11 @@ for i in range(Paddind_Head_size+1, Paddind_Head_size + Test_Sample_size*2 + 1, 
 
     print(samples_after)
     print(samples_before)
-    #Test_Sample_out[i] = np.add(samples_after, np.flip(samples_before))
+    Test_Sample_out[i-1] = Test_Sample_in_spread[i-1]
+    Test_Sample_out[i] = np.sum(np.multiply(np.add(samples_after, np.flip(samples_before)), FIR_Coefs))/FIR_Norm_Coef
 
 # Plots
 pyplot.figure(1)
 pyplot.plot(Test_Sample_in_spread, '.')
-pyplot.plot(Test_Sample_out, '.')
+pyplot.plot(Test_Sample_out)
 pyplot.show()
