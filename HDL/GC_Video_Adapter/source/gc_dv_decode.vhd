81ebc268d8f15dce537d593aefbc6bd88e6b8b3c
Nuno Sousa <nunosousa@outlook.com>
2021-01-19 18:15:59 +0000

    Update gc_dv_decode.vhd

--- HDL/GC_Video_Adapter/source/gc_dv_decode.vhd
+++ HDL/GC_Video_Adapter/source/gc_dv_decode.vhd
@@ -59,11 +59,7 @@ begin
 			Blanking <= '0';
 		elsif (rising_edge(vclk)) then
 			-- Store new vdata sample and shift samples
-			--vdata_buffer(0) <= vdata_buffer(1);
-			--vdata_buffer(1) <= vdata_buffer(2);
-			--vdata_buffer(2) <= vdata_buffer(3);
-			--vdata_buffer(3) <= vdata;
-			vdata_buffer <= vdata & vdata_buffer(0 to 2);
+			vdata_buffer <= vdata_buffer(1 to 3) & vdata;
 			
 			if (vsample_count < 5) then
 				vsample_count <= vsample_count + 1;
