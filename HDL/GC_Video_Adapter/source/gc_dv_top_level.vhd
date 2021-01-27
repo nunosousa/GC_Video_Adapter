5c700e9ea77c83dd600e4f54cf4f18b81e387333
Nuno Sousa <nunosousa@users.noreply.github.com>
2021-01-27 12:37:10 +0000

    Update gc_dv_top_level.vhd

--- HDL/GC_Video_Adapter/source/gc_dv_top_level.vhd
+++ HDL/GC_Video_Adapter/source/gc_dv_top_level.vhd
@@ -14,14 +14,5 @@ end entity;
 architecture behav of gc_dv_top_level is
 	
 begin
-	process : process(pclk)
-	begin
-		if (reset = '1') then
-			-- Reset something
-		elsif (rising_edge(pclk)) then
-			if (dvalid_in = '1') then
-			
-			end if;
-		end if;	-- if (reset = '1')
-	end process;
+
 end behav;
