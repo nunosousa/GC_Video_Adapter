a623ade81644689eab343830fcc971af931a3944
Nuno Sousa <nunosousa@outlook.com>
2021-02-14 11:20:50 +0000

    Update gc_dv_YCbCr_to_RGB.vhd

--- HDL/GC_Video_Adapter/source/gc_dv_YCbCr_to_RGB.vhd
+++ HDL/GC_Video_Adapter/source/gc_dv_YCbCr_to_RGB.vhd
@@ -6,9 +6,23 @@ use ieee.numeric_std.all;
 
 entity gc_dv_YCbCr_to_RGB is
 	port(
-
+		Y			: in	std_logic_vector(7 downto 0);
+		Cb			: in	std_logic_vector(7 downto 0);
+		Cr			: in	std_logic_vector(7 downto 0);
+		H_sync		: in	std_logic;
+		V_sync		: in	std_logic;
+		C_sync		: in	std_logic;
+		Blanking	: in	std_logic;
+		dvalid		: in	std_logic,
+		R_out		: out	std_logic_vector(7 downto 0);
+		G_out		: out	std_logic_vector(7 downto 0);
+		B_out		: out	std_logic_vector(7 downto 0);
+		H_sync_out	: out	std_logic;
+		V_sync_out	: out	std_logic;
+		C_sync_out	: out	std_logic;
+		Blanking_out: out	std_logic;
+		dvalid_out	: out	std_logic
 	);
-	
 end entity;
 
 architecture behav of gc_dv_YCbCr_to_RGB is
@@ -16,12 +30,8 @@ architecture behav of gc_dv_YCbCr_to_RGB is
 begin
 	process : process(pclk)
 	begin
-		if (reset = '1') then
-			-- Reset something
-		elsif (rising_edge(pclk)) then
-			if (dvalid_in = '1') then
-			
-			end if;
-		end if;	-- if (reset = '1')
+		if (rising_edge(pclk)) then
+		
+		end if;	-- if (rising_edge(pclk))
 	end process;
 end behav;
